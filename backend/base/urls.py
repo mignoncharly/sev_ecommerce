d82781555712b31bf02a94f0fc5dafe588dd8d80
mignoncharly <mignoncharly@yahoo.fr>
2021-03-11 12:40:17 +0100

    first commit

---
+++ backend/base/urls.py
@@ -0,0 +1,10 @@
+# connection the views to urls
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.getRoutes, name="routes"),
+    path('products/', views.getProducts, name="products"),
+    path('products/<str:pk>/', views.getProduct, name="product"),
+]
