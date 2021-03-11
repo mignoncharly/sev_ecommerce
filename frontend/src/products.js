e973cc0a323725b50da91d7e92346fd9f2154139
mignoncharly <mignoncharly@yahoo.fr>
2021-03-11 14:46:22 +0100

    Add files via upload

---
+++ frontend/src/products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    '_id': '1',
+    'name': 'Mask 1',
+    'image': '/images/m4.jpeg',
+    'description':
+      'No description yet',
+    'brand': 'Charis Et Chik',
+    'category': 'Health',
+    'price': 19.99,
+    'countInStock': 200,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'Mask 2',
+    'image': '/images/m1.jpeg',
+    'description':
+      'No description yet',
+    'brand': 'Charis Et Chik',
+    'category': 'Health',
+    'price': 19.99,
+    'countInStock': 7,
+    'rating': 4.0,
+    'numReviews': 8,
+  },
+  {
+    '_id': '3',
+    'name': 'Mask 3',
+    'image': '/images/m2.jpeg',
+    'description':
+      'No description yet',
+    'brand': 'Charis Et Chik',
+    'category': 'Health',
+    'price': 19.99,
+    'countInStock': 5,
+    'rating': 3,
+    'numReviews': 12,
+  },
+  {
+    '_id': '4',
+    'name': 'Mask 4',
+    'image': '/images/m3.jpeg',
+    'description':
+      'No description yet',
+    'brand': 'Charis Et Chik',
+    'category': 'Health',
+    'price': 19.99,
+    'countInStock': 11,
+    'rating': 5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '5',
+    'name': 'Mask 5',
+    'image': '/images/m5.jpeg',
+    'description':
+      'No description yet',
+    'brand': 'Charis Et Chik',
+    'category': 'Health',
+    'price': 19.99,
+    'countInStock': 7,
+    'rating': 3.5,
+    'numReviews': 10,
+  },
+  {
+    '_id': '6',
+    'name': 'Mask 6',
+    'image': '/images/m6.jpeg',
+    'description':
+      'No description yet!',
+    'brand': 'Charis Et Chik',
+    'category': 'Health',
+    'price': 19.99,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+]
+
+
+export default products
